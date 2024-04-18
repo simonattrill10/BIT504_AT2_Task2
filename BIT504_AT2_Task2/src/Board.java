f6939f50fe648533bd4fbd39171df20a30f73fdc
simonattrill10 <simonattrill10@gmail.com>
2024-04-18 19:26:38 +1200

    Board
    -Initialized cell array to create board in GameMain

--- BIT504_AT2_Task2/src/Board.java
+++ BIT504_AT2_Task2/src/Board.java
@@ -13,7 +13,7 @@ public class Board {
 	public Board() {
 		
 	 //TODO: initialise the cells array using ROWS and COLS constants 
-
+	cells = new Cell[GameMain.ROWS][GameMain.COLS];
 		
 		for (int row = 0; row < GameMain.ROWS; ++row) {
 			for (int col = 0; col < GameMain.COLS; ++col) {
